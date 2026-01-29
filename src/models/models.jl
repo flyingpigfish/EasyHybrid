5dcc3564ca9dee45e6de3a6eaba6d22336b729af
Lazaro Alonso <lazarus.alon@gmail.com>
2026-01-29 19:04:21 +0100

    delete (#222)

--- src/models/models.jl
+++ src/models/models.jl
@@ -1,12 +1,5 @@
 abstract type EasyHybridModels end
 
-# include("neural_networks.jl")
-# include("LinearHybridModel.jl")
-# include("LinearHybridModel_2outputs.jl")
-# include("FluxPartModel_NEE_ET2.jl")
-# include("FluxPartModel_Q10.jl")
-# include("FluxPartModel_Q10_Lux.jl")
-# include("SinusHybridModel.jl")
 include("LinearHM.jl")
 include("Respiration_Rb_Q10.jl")
 include("Rs_components.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,5 @@
-# using Pkg
-# Pkg.activate(@__DIR__)
-# using Revise
 using EasyHybrid
 using Test
-# import Flux
-
-dk = gen_linear_data()
-dk_twos = gen_linear_data_2outputs()
-#lhm_twos = LinearHybridModel_2outputs([:x2, :x3], [:x1, :x2], 1, 5, DenseNN; b=[4.0f0])
-#out_twos = lhm_twos(dk_twos, :infer)
 
 # Include GenericHybridModel tests
 include("test_generic_hybrid_model.jl")
@@ -30,38 +21,4 @@ include("test_wrap_tuples.jl")
     @test lhm.forcing == [:x1]
     @test lhm.β == [1.5f0]
     @test lhm.predictors == [:x2, :x3]
-end
-
-@testset "EasyHybrid.jl" begin
-    # test model instantiation
-    # lhm = LinearHybridModel([:x2, :x3], [:x1], 1, 5; b=[2.0f0])
-    # @test lhm.forcing == [:x1]
-    # @test lhm.b == [2.0f0]
-    # @test lhm.predictors == [:x2, :x3]
-    # @test typeof(lhm.DenseLayers) <: Flux.Chain
-
-    # # test model coupling
-    # out_lhm = lhm(dk, :infer)
-    # @test size(out_lhm.a) == (1, 1000)
-    # @test size(out_lhm.pred) == (1000, 1000)
-
-    # # test two_outputs
-    # lhm_twos = LinearHybridModel_2outputs([:x2, :x3], [:x1, :x2], 1, 5, DenseNN; b=[4.0f0])
-    # @test lhm_twos.a == [1.0f0]
-    # @test lhm_twos.b == [4.0f0]
-    # out_twos = lhm_twos(dk_twos, :infer)
-    # @test length(out_twos) == 3
-    #test model output
-
-    # test model instantiation
-    # fpmod = FluxPartModel_NEE_ET2([:TA_F, :SW_IN_F], [:doy], [:VPD_F, :hour], [:doy])
-    # @test typeof(fpmod) <: FluxPartModel_NEE_ET2
-    # # test model instantiation
-    # Q10_m = FluxPartModel_Q10([:TA, :VPD], [:SW_POT_sm_diff, :SW_POT_sm]; Q10=[2.0f0])
-    # @test Q10_m.Q10 == [2.0f0]
-
-    # # test model instantiation
-    # sinus_m = SinusHybridModel([:x1], [:x1], 1; b=[0.0f0])
-    # @test sinus_m.b == [0.0f0]
-
 end
